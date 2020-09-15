daa0271a9eebf19eadba3d72d8624f77043a1bc7
Stanislav QQ <stanislavqq@yandex.ru>
2020-09-15 16:49:25 +0500

    Init

---
+++ src/App.svelte
@@ -0,0 +1,51 @@
+<script>
+	import ColorBlock from './ColoredBlock.svelte';
+	let blocks;
+	blocks = [
+		{id: 1},
+		{id: 2, color: 'red', opacity: 1},
+		{id: 3, color: 'blue', opacity: 1},
+		{id: 4, color: 'green', opacity: 1},
+	];
+
+	function clickBlock(id){
+		let index = blocks.findIndex((item) => item.id === id);
+		blocks.filter(item => item.id !== id).forEach(item => item.opacity = 1);
+
+		if (blocks[index].opacity > 0) {
+			blocks[index].opacity = 0;
+		} else {
+			blocks[index].opacity = 1;
+		}
+	}
+</script>
+
+<main>
+	<div class="container">
+		{#each blocks as block (block.id)}
+			<ColorBlock on:click={clickBlock(block.id)} bind:color={block.color} bind:opacity={block.opacity}/>
+		{/each}
+	</div>
+</main>
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	h1 {
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
---
+++ src/ColoredBlock.svelte
@@ -0,0 +1,18 @@
+<script>
+    import { createEventDispatcher } from 'svelte';
+
+    const dispatch = createEventDispatcher();
+
+    export let opacity = 1;
+    export let color = 'gray';
+</script>
+
+<style>
+    .color-block {
+        display: inline-block;
+        width: 100px;
+        height: 100px;
+    }
+</style>
+
+<div on:click class="color-block" style="background-color: {color}; opacity: {opacity}"></div>
